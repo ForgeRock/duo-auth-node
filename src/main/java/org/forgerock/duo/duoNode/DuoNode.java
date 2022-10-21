a07622c93ad25452b256ba71bd408e2693eff1ec
TylerGelinas <=>
2022-10-21 12:37:05 -0500

    Remove mfa tag and add more exception handling

--- src/main/java/org/forgerock/duo/duoNode/DuoNode.java
+++ src/main/java/org/forgerock/duo/duoNode/DuoNode.java
@@ -35,9 +35,16 @@ import org.forgerock.openam.auth.node.api.*;
 import org.forgerock.openam.core.CoreWrapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.forgerock.json.JsonValue;
+import java.util.Arrays;
+import org.forgerock.util.i18n.PreferredLocales;
+import java.util.Collections;
+import org.forgerock.openam.auth.node.api.OutcomeProvider;
+import java.util.List;
+import java.util.ResourceBundle;
 
-@Node.Metadata(outcomeProvider = AbstractDecisionNode.OutcomeProvider.class,
-        configClass = DuoNode.Config.class, tags = {"mfa", "multi-factor authentication", "marketplace", "trustnetwork"})
+@Node.Metadata(outcomeProvider = DuoNode.OutcomeProvider.class,
+        configClass = DuoNode.Config.class, tags = {"multi-factor authentication", "marketplace", "trustnetwork"})
 public class DuoNode extends AbstractDecisionNode {
 
     private final Logger logger = LoggerFactory.getLogger("amAuth");
@@ -90,20 +97,27 @@ public class DuoNode extends AbstractDecisionNode {
 
     @Override
     public Action process(TreeContext context) throws NodeProcessException {
-        String username = context.sharedState.get(SharedStateConstants.USERNAME).asString().toLowerCase();
+        try {
+            String username = context.sharedState.get(SharedStateConstants.USERNAME).asString().toLowerCase();
 
-        if (context.hasCallbacks()) {
-            logger.debug(loggerPrefix + "Duo Callbacks Received");
-            String signatureResponse = context.getCallback(HiddenValueCallback.class).get().getValue();
-            try {
-                return goTo(username.equals(DuoWeb.verifyResponse(iKey, sKey, aKey, signatureResponse))).build();
-            } catch (DuoWebException | NoSuchAlgorithmException | InvalidKeyException | IOException e) {
-                logger.error(loggerPrefix+ ":");
-                e.printStackTrace();
-                return goTo(false).build();
+            if (context.hasCallbacks()) {
+                logger.debug(loggerPrefix + "Duo Callbacks Received");
+                String signatureResponse = context.getCallback(HiddenValueCallback.class).get().getValue();
+                try {
+                    return Action.goTo(String.valueOf(username.equals(DuoWeb.verifyResponse(iKey, sKey, aKey, signatureResponse)))).build();
+                } catch (DuoWebException | NoSuchAlgorithmException | InvalidKeyException | IOException e) {
+                    logger.error(loggerPrefix+ ":");
+                    e.printStackTrace();
+                    return Action.goTo("false").build();
+                }
             }
+            return buildCallbacks(username);
+        } catch(Exception ex) {
+            logger.error(loggerPrefix + "Exception occurred");
+            ex.printStackTrace();
+            context.sharedState.put("Exception", ex.toString());
+            return Action.goTo("error").build();
         }
-        return buildCallbacks(username);
     }
 
     private Action buildCallbacks(String username) {
@@ -113,6 +127,32 @@ public class DuoNode extends AbstractDecisionNode {
                     String.format(INIT_SCRIPT, apiHostName, DuoWeb.signRequest(iKey, sKey, aKey, username))));
             add(new HiddenValueCallback("duo_response"));
         }}).build();
+    }
+
+    public static final class OutcomeProvider implements org.forgerock.openam.auth.node.api.OutcomeProvider {
+        /**
+         * Outcomes Ids for this node.
+         */
+        static final String SUCCESS_OUTCOME = "true";
+        static final String ERROR_OUTCOME = "error";
+        static final String FALSE_OUTCOME = "false";
+        private static final String BUNDLE = DuoNode.class.getName();
+
+        @Override
+        public List<Outcome> getOutcomes(PreferredLocales locales, JsonValue nodeAttributes) {
+
+            ResourceBundle bundle = locales.getBundleInPreferredLocale(BUNDLE, OutcomeProvider.class.getClassLoader());
+
+            List<Outcome> results = new ArrayList<>(
+                    Arrays.asList(
+                            new Outcome(SUCCESS_OUTCOME, "True")
+                    )
+            );
+            results.add(new Outcome(FALSE_OUTCOME, "False"));
+            results.add(new Outcome(ERROR_OUTCOME, "Error"));
+
+            return Collections.unmodifiableList(results);
+        }
     }
 
 }
