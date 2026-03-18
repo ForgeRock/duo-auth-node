6ebaf82b2d1749305c6d9774e8aa9efeba2ea26d
TylerGelinas <tyler.gelinas@forgerock.com>
2026-03-18 12:07:27 -0500

    7.3.1 version pom.xml

--- src/main/java/org/forgerock/duo/duoNode/DuoNodePlugin.java
+++ src/main/java/org/forgerock/duo/duoNode/DuoNodePlugin.java
@@ -59,7 +59,7 @@ import org.forgerock.openam.plugins.PluginException;
  */
 public class DuoNodePlugin extends AbstractNodeAmPlugin {
 
-	static private String currentVersion = "2.3.17";
+	static private String currentVersion = "2.3.20";
 	static final String logAppender = "[Version: " + currentVersion + "][Marketplace] ";
 	
     /** 
